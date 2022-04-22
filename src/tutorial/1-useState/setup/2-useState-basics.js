c3359872420dc20d0ac4437bf9580ba55d5aa9dc
jhonnycave <luis.j.cueva@gmail.com>
2022-04-22 11:44:51 -0500

    1-useState-modulo-02

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  /*  */
+  const [text, setText] = useState("random title");
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("Hello worlds");
+    } else {
+      setText("random title");
+    }
+  };
+
+  /*  */
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className="btn" type="button" onClick={handleClick}>
+        Changle Title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
