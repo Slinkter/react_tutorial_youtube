640793cba0dd5b7b8b96a7860d5f52fe8175c394
jhonnycave <luis.j.cueva@gmail.com>
2022-04-25 16:30:31 -0500

    7 : fin

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
@@ -6,7 +6,29 @@ const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const [text, setText] = useState(""); // text : ''  , setText : 'sadasdasd '
+  const [isError, setIsError] = useState(false); // isError : false , setIsError: True o false
+
+  return (
+    <>
+      <h1>{text || "no name "} </h1>
+
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Aplicar Error
+      </button>
+
+      {isError && <h1>Error...</h1>}
+      {console.log(isError)}
+
+      {isError ? (
+        <p> there is an error ...</p>
+      ) : (
+        <div>
+          <p> there is no error </p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,41 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.group('handleSubmit')
+    console.log('refContainer.current.value');
+    console.log(refContainer.current.value);
+    console.groupEnd()
+  };
+
+  useEffect(() => {
+    console.group('UseEffect')
+    refContainer.current.focus();
+    console.log(' refContainer.current.focus');
+    console.log(refContainer.current);
+    console.groupEnd()
+  });
+
+  return (
+    <>
+      <h2>useRef</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" className="" ref={refContainer} />
+          <button type="submit" className="btn ">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default UseRefBasics;
