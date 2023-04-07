98f231284a3a49090f1603a04c4fe0eb3e3c6018
slinkter <luis.j.cueva@gmail.com>
2023-04-07 13:09:56 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Setup from "./tutorial/12-memo-useMemo-useCallback/final/index";
 function App() {
     return (
         <div className="container">
+ 
             <Setup />
         </div>
     );
 }
 
 export default App;
+
