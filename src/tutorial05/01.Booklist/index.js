19b68fd3e9705ea0479481ecde48387f415b4964
jhonnycave <luis.j.cueva@gmail.com>
2023-04-08 14:19:05 -0500

    macos ok

--- src/tutorial05/01.Booklist/index.js
+++ src/tutorial05/01.Booklist/index.js
@@ -1,1 +1,24 @@
 // 4 :20 : 10
+import React from "react";
+
+const Author = () => <h1>Author</h1>;
+const Imagen = () => <h2>imagne placeholder</h2>;
+const Title = () => {
+  return <h2>Book title </h2>;
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Imagen></Imagen>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  );
+};
+
+const index = () => {
+  return <div>index</div>;
+};
+
+export default index;
