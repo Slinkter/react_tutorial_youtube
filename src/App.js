c9d330f537a899c6bfbf562b6eae88a060de6fac
jhonnycave <luis.j.cueva@gmail.com>
2023-05-02 11:15:08 -0500

    macos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Setup from "./tutorial/12-redux-toolkit/main";
 function App() {
-  return <div className="container">{/*  <Setup /> */}</div>;
+  return <div className="container">{<Setup />}</div>;
 }
 
 export default App;
