dbe7a269dfe2f03c5342d2f518b0a9c177b9ebca
slinkter <luis.j.cueva@gmail.com>
2023-05-14 22:45:58 -0500

    win 11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/6-forms/04-other-inputs";
+import Setup from "./tutorial/7-useRef/1-useRef-basics";
 function App() {
   return (
     <>
--- src/tutorial/7-useRef/1-useRef-basics.js
+++ src/tutorial/7-useRef/1-useRef-basics.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const initial = null;
+const initValue = 0;
+const initBoolean = false;
 
 const UseRefBasics = () => {
   //
+  const [value, setValue] = useState(initValue);
   const refContainer = useRef(initial);
+  const isMounted = useRef(initBoolean);
   //
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("handleSubmit", refContainer.current.value);
+    console.log(refContainer.current);
+    const name = refContainer.current.value;
+    console.log(name);
+    console.log("refContainer.current.value : ", refContainer.current.value);
   };
   //
   useEffect(() => {
@@ -16,14 +23,38 @@ const UseRefBasics = () => {
     console.log("useEffect", refContainer.current);
   });
   //
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    console.log("re-render");
+    return () => {};
+  }, [value]);
+
   return (
     <>
       <form className="form" onSubmit={handleSubmit}>
-        <div>
-          <input type="text" ref={refContainer} />
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            {" "}
+            name
+          </label>
+          <input
+            type="text"
+            ref={refContainer}
+            id="name"
+            className="form-input"
+          />
         </div>
-        <button type="submit">submit</button>
+        <button className="btn btn-block" type="submit">
+          submit
+        </button>
       </form>
+      <h1>value : {value}</h1>
+      <button onClick={() => setValue(value + 1)} className="btn">
+        increase
+      </button>
     </>
   );
 };
